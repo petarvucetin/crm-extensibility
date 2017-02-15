3ef37cad9508fceb09fbcd64f4c8813e825ee84c
petar <petar.vucetin@clear-lines.com>
2017-02-15 14:01:34 -0800

    cleanup

--- src/ArcherInc/ArcherIncPlugin/AccountControllerPlugin.cs
+++ src/ArcherInc/ArcherIncPlugin/AccountControllerPlugin.cs
@@ -11,6 +11,7 @@ namespace ArcherIncPlugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
+            #region extract services
             var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
             var executionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
@@ -18,23 +19,15 @@ namespace ArcherIncPlugin
             var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             
             var endpointNotificationService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
+            #endregion
 
             try
             {
                 tracingService.Trace("AccountControllerPlugin plugin started.");
 
-                //send message to the queue
-                var serviceEndpointId = new Guid("3dce31eb-7cf1-e611-80c5-00155d07290a");
+                SendContextToQueue(executionContext, endpointNotificationService, tracingService);
 
-                var azureQueue = new EntityReference("serviceendpoint", serviceEndpointId);
-
-                endpointNotificationService.Execute(azureQueue, executionContext);
-
-                //send message to Function
-                tracingService.Trace("*** FUNCTION START ****");
-                var response = Demo.CallAzureFunction(executionContext, tracingService);
-                tracingService.Trace("response -> " + response);
-                tracingService.Trace("*** FUNCTION END ****");
+                SendContextToAzureFunction(executionContext, tracingService);
 
                 tracingService.Trace("AccountControllerPlugin plugin completed.");
             }
@@ -43,6 +36,30 @@ namespace ArcherIncPlugin
                 tracingService.Trace("Exception: {0}", e.ToString());
                 throw;
             }
+
+        }
+
+        private void SendContextToAzureFunction(IPluginExecutionContext executionContext,
+            ITracingService tracingService)
+        {
+            //send message to Function
+            tracingService.Trace("*** FUNCTION START ****");
+            var response = Demo.CallAzureFunction(executionContext, tracingService);
+            tracingService.Trace("response -> " + response);
+            tracingService.Trace("*** FUNCTION END ****");
+
+        }
+
+        private void SendContextToQueue(IPluginExecutionContext executionContext, 
+            IServiceEndpointNotificationService endpointNotificationService,
+            ITracingService tracingService)
+        {
+            //send message to the queue
+            var serviceEndpointId = new Guid("3dce31eb-7cf1-e611-80c5-00155d07290a");
+
+            var azureQueue = new EntityReference("serviceendpoint", serviceEndpointId);
+
+            endpointNotificationService.Execute(azureQueue, executionContext);
 
         }
     }

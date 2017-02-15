66f6ac557b0cfa8d1a194093ca43aa86d7fb0f79
petar <petar.vucetin@clear-lines.com>
2017-02-15 09:27:23 -0800

    initial

---
+++ src/ArcherInc/ArcherIncPlugin/AccountControllerPlugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace ArcherIncPlugin
+{
+    public class AccountControllerPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            var executionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            
+            var endpointNotificationService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
+
+            try
+            {
+                tracingService.Trace("AccountControllerPlugin plugin started.");
+
+                //send message to the queue
+                var serviceEndpointId = new Guid("3dce31eb-7cf1-e611-80c5-00155d07290a");
+
+                var azureQueue = new EntityReference("serviceendpoint", serviceEndpointId);
+
+                endpointNotificationService.Execute(azureQueue, executionContext);
+
+                //send message to Function
+                tracingService.Trace("*** FUNCTION START ****");
+                var response = Demo.CallAzureFunction(executionContext, tracingService);
+                tracingService.Trace("response -> " + response);
+                tracingService.Trace("*** FUNCTION END ****");
+
+                tracingService.Trace("AccountControllerPlugin plugin completed.");
+            }
+            catch (Exception e)
+            {
+                tracingService.Trace("Exception: {0}", e.ToString());
+                throw;
+            }
+
+        }
+    }
+}
---
+++ src/ArcherInc/ArcherIncPlugin/Demo.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcherIncPlugin
+{
+    public static class Demo
+    {
+        public static string ToJSon(this IExecutionContext context)
+        {
+            var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(IExecutionContext));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.WriteObject(ms, context);
+                return Encoding.Default.GetString(ms.ToArray());
+            }
+        }
+
+        public static string CallAzureFunction(IExecutionContext context, ITracingService tracingService)
+        {
+            try
+            {
+                var ctx = context.ToJSon();
+
+                var webAddress = new Uri("https://crm-archerinc.azurewebsites.net/api/Crm-ArcherIncHttp?code=Ggahdjw10AwqqgxmQmgQd/As1mx3mPtHTMVTgoc1ptOrjCMnu1MSOg==");
+
+                var postData = "{ 'name' : 'CRM Plugin' }";
+
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers["Content-Type"] = "application/json; charset=utf-8";
+                    var response = client.UploadString(webAddress, ctx);
+                    return response;
+                }
+            }
+
+            catch (WebException exception)
+            {
+                string str = string.Empty;
+                if (exception.Response != null)
+                {
+                    using (StreamReader reader =
+                        new StreamReader(exception.Response.GetResponseStream()))
+                    {
+                        str = reader.ReadToEnd();
+                    }
+                    exception.Response.Close();
+                }
+                if (exception.Status == WebExceptionStatus.Timeout)
+                {
+                    throw new InvalidPluginExecutionException(
+                        "The timeout elapsed while attempting to issue the request.", exception);
+                }
+                throw new InvalidPluginExecutionException(String.Format(CultureInfo.InvariantCulture,
+                    "A Web exception occurred while attempting to issue the request. {0}: {1}",
+                    exception.Message, str), exception);
+            }
+        }
+    }
+}
